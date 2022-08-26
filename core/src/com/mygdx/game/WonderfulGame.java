e47571dd03cf2e18603872d572da2da97c838da9
mvorobyev <ptichkinm@mail.ru>
2022-08-26 20:34:06 +0300

    homework_2. Создал класс кастомной анимации, реализовал движение и коллизии с границами окна

--- core/src/com/mygdx/game/WonderfulGame.java
+++ core/src/com/mygdx/game/WonderfulGame.java
@@ -2,51 +2,63 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import static com.badlogic.gdx.Gdx.graphics;
 import static com.badlogic.gdx.Gdx.input;
 
 public class WonderfulGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	private static long clicks;
-
-	TextureRegion[][] regions0;
-
+	private SpriteBatch batch;
+	private CustomAnimation animation;
+	private String direction;	// Направление движения
+	private int xPosition;	// Положение персонажа на жкране
+	private float speed;	// Базовая скорость движения
+	private float realSpeed;	// Действующая скорость движения
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("monster.png");
-
-		int xTileWidth = img.getWidth() / 9;
-		int yTileWidth = img.getHeight() / 8;
-
-		TextureRegion region0 = new TextureRegion(img);
-		regions0 = region0.split(xTileWidth, yTileWidth);
+		animation = new CustomAnimation("img_1.png", 6, 1, Animation.PlayMode.LOOP);
+		direction = "right";
+		speed = 8.0f;
+		realSpeed = speed;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
+		ScreenUtils.clear(1, 1, 1, 1);
 
-//		int mouseX = input.getX() - img.getWidth() / 2;
-//		int mouseY = graphics.getHeight() - input.getY() - img.getHeight() / 2;
-//		if (input.isButtonJustPressed(Input.Buttons.LEFT)) {
-//			clicks++;
-//			graphics.setTitle("Left mouse pressed " + clicks + " times");
-//		}
-		batch.draw(regions0[1][1], 0, 0);
+		animation.setTime(graphics.getDeltaTime());
+
+		boolean rightBorderCollision = "right".equals(direction) && realSpeed > 0 && xPosition >= (graphics.getWidth() - animation.getFrame().getRegionWidth());
+		boolean leftBorderCollision = "left".equals(direction) && realSpeed < 0 && xPosition <= 0;
+
+		// Управление направлением движения
+		if (rightBorderCollision || input.isKeyJustPressed(Input.Keys.LEFT)) {
+			direction = "left";
+		} else if (leftBorderCollision || input.isKeyJustPressed(Input.Keys.RIGHT)) {
+			direction = "right";
+		}
+
+		// Меняем направление персонажа
+		if (!animation.getFrame().isFlipX() && "left".equals(direction)	// Повернуть влево
+				|| animation.getFrame().isFlipX() && "right".equals(direction)) {	// Повернуть вправо
+			animation.getFrame().flip(true, false);
+		}
+
+
+		realSpeed = "right".equals(direction) ? speed : -speed;
+		xPosition += realSpeed;
+
+		batch.begin();
+		batch.draw(animation.getFrame(), xPosition, 0);
 		batch.end();
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		animation.dispose();
 	}
 }
