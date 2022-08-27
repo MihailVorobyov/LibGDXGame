e8ce14b8be113f001c3ae980911d9ed67b52915f
mvorobyev <ptichkinm@mail.ru>
2022-08-27 19:21:13 +0300

    homework_3. Создан новый класс игры Main, экраны игры и меню. Реализовано переключение между экранами по нажатию любой клавиши.

---
+++ core/src/com/mygdx/game/screens/MenuScreen.java
@@ -0,0 +1,66 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.Main;
+
+public class MenuScreen implements Screen {
+    private final Main game;
+    private final SpriteBatch batch;
+    private final Texture img;
+
+    public MenuScreen(Main game) {
+        this.game = game;
+        batch = new SpriteBatch();
+        img = new Texture("menu.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.ORANGE);
+
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
+            dispose();
+            game.setScreen(new GameScreen(game));
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
